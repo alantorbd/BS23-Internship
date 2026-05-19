2ec631e506a0adefe686699853d3e3e93f01335b
Antor <alantorbd@gmail.com>
2026-05-19 14:03:20 +0600

    initialization first project

---
+++ BasicOperation/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicOperation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            DataTypes dataTypes = new DataTypes();
+            dataTypes.RunTask();
+
+
+
+
+        }
+    }
+}
