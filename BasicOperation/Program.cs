bd51c510de71e39700867918c41183db475124f7
Antor <alantorbd@gmail.com>
2026-05-19 21:36:35 +0600

    Study loops in c#

--- BasicOperation/Program.cs
+++ BasicOperation/Program.cs
@@ -11,7 +11,7 @@ namespace BasicOperation
         static void Main(string[] args)
         {
             //Console.WriteLine("Hello World!");
-            FlowControl flowControl = new FlowControl();
+            Loops loops = new Loops();
 
             Console.ReadLine();
 
