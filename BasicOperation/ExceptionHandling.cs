936a9699b74e4698ec3e8c0a2b0ad27d77dbf6b0
Antor <alantorbd@gmail.com>
2026-05-20 10:56:23 +0600

    Study Excepetion Handling operation

---
+++ BasicOperation/ExceptionHandling.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicOperation
+{
+    internal class ExceptionHandling
+    {
+        public static void TestExceptionHandling()
+        {
+            Console.Write("Enter a number: ");
+
+            try
+            {
+                int num = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine(num);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Input is not a valid number.");
+            }
+            catch(OverflowException)
+            {
+                Console.WriteLine("Input number is too large or too small.");
+            }
+            catch (ArgumentNullException)
+            {
+                Console.WriteLine("Input cannot be null.");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Error Ocurre: {e.Message}");
+            }
+        }
+    }
+}
