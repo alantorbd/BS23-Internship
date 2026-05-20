1cca0c97fdd7102142007f346358f6fa79c42fc7
Antor <alantorbd@gmail.com>
2026-05-20 15:52:22 +0600

    initailaization code and add starting code

---
+++ FareEngineAssessment/FareEngineAssessment/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FareEngineAssessment
+{
+
+    public enum TripStatus { Pending, Paid, Failed }
+    public interface IPromotion
+    {
+        decimal ApplyDiscount(decimal currentFare);
+    }
+    public interface IPaymentService
+    {
+        bool ProcessPayment(string passengerId, decimal amount);
+    }
+    public abstract class Vehicle
+    {
+        // Candidate to implement properties and encapsulation
+    }
+    public class Trip
+    {
+        // Candidate to implement domain logic, calculations, and abstraction 
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Candidate should write a small test suite here to demonstrate 
+            // their code works under various test cases (Standard vs Luxury, with/
+
+        }
+    }
+
+
+}
