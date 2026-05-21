3e324d657ccddfe4c9eb50c73810f9646d906b02
Antor <alantorbd@gmail.com>
2026-05-21 17:20:21 +0600

    Study generics operation and constraints

--- BasicOperation/Program.cs
+++ BasicOperation/Program.cs
@@ -11,7 +11,7 @@ namespace BasicOperation
         static void Main(string[] args)
         {
             //Console.WriteLine("Hello World!");
-            CollectionOperations.TestCollectionOperations();
+            GenericsOperations.TestGenericsOperations();
 
 
 
